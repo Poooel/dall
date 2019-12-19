fbb6cc2ce3defee463cdd3877c0258921adedc3b
Alexis Lucas <alexis.lucas1@viacesi.fr>
2019-12-19 10:57:47 +0100

    Fixed NPE when no loaded ad is available in the sheet

--- src/main/java/com/dall/service/DuplicateService.java
+++ src/main/java/com/dall/service/DuplicateService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,11 +23,9 @@ public class DuplicateService {
     private final DallConfiguration dallConfiguration;
 
     @Autowired
-    public DuplicateService(
-        Sheets sheets,
+    public DuplicateService(Sheets sheets,
         GoogleCredentialsConfiguration googleCredentialsConfiguration,
-        DallConfiguration dallConfiguration
-    ) {
+        DallConfiguration dallConfiguration) {
         this.spreadsheet = sheets.spreadsheets();
         this.googleCredentialsConfiguration = googleCredentialsConfiguration;
         this.dallConfiguration = dallConfiguration;
@@ -35,43 +35,46 @@ public class DuplicateService {
 
     @SneakyThrows
     private Set<String> loadExistingLinks() {
-        ValueRange resultFromCityCentre = this.spreadsheet
-            .values()
-            .get(
-                googleCredentialsConfiguration.getSpreadsheetId(),
-                String.format(
-                    "%s!%s%d:%s%d",
+        ValueRange resultFromCityCentre = this.spreadsheet.values()
+            .get(googleCredentialsConfiguration.getSpreadsheetId(),
+                String.format("%s!%s%d:%s%d",
                     dallConfiguration.getCityCentreSheetName(),
                     dallConfiguration.getStartingColumnCityCentre(),
                     dallConfiguration.getStartingCellCityCentre(),
                     dallConfiguration.getStartingColumnCityCentre(),
-                    100
-                )
-            )
+                    100))
             .execute();
 
-        ValueRange resultFromSuburb = this.spreadsheet
-            .values()
-            .get(
-                googleCredentialsConfiguration.getSpreadsheetId(),
-                String.format(
-                    "%s!%s%d:%s%d",
+        ValueRange resultFromSuburb = this.spreadsheet.values()
+            .get(googleCredentialsConfiguration.getSpreadsheetId(),
+                String.format("%s!%s%d:%s%d",
                     dallConfiguration.getSuburbSheetName(),
                     dallConfiguration.getStartingColumnSuburb(),
                     dallConfiguration.getStartingCellSuburb(),
                     dallConfiguration.getStartingColumnSuburb(),
-                    100
-                )
-            )
+                    100))
             .execute();
 
-        return Stream.concat(
-            resultFromCityCentre.getValues().stream(),
-            resultFromSuburb.getValues().stream()
-        )
-            .flatMap(Collection::stream)
-            .map(Object::toString)
-            .collect(Collectors.toSet());
+        return streamToSet(valueRangesToStream(resultFromCityCentre, resultFromSuburb));
+    }
+
+    private Stream<List<Object>> valueRangesToStream(ValueRange first, ValueRange second) {
+        if(first.getValues() != null && second.getValues() != null) {
+            return Stream.concat(first.getValues().stream(), second.getValues().stream());
+        } else if(first.getValues() != null) {
+            return first.getValues().stream();
+        } else if(second.getValues() != null) {
+            return second.getValues().stream();
+        } else {
+            return null;
+        }
+    }
+
+    private Set<String> streamToSet(Stream<List<Object>> stream) {
+        if(stream != null) {
+            return stream.flatMap(Collection::stream).map(Object::toString).collect(Collectors.toSet());
+        }
+        return new HashSet<>();
     }
 
     public boolean exists(String link) {
